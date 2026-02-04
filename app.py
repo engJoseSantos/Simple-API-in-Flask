620ceabbba748df8eabdc174b9c354872b28b010
jose santos <eng.josesantos97@gmail.com>
2026-02-04 20:29:47 +0100

    Add: a class to store data

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask
 
+class Crypto:
+    def __init__(self, id, name, code, value_to_usd):
+        self.id = id
+        self.name = name
+        self.code = code
+        self.value_to_usd = value_to_usd
+    
+    def to_dict(self):
+        return {
+            "id" : self.id,
+            "name" :self.name,
+            "code" :self.code,
+            "value" :self.value_to_usd
+        }
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,4 +25,6 @@ def info_python():
     return "Python is a high-level, general-purpose programming language."
 
 if __name__ == "__main__":
+    btc = Crypto(1, "Bitcoin", "BTC", 73550.97)
+    print(btc.to_dict())
     app.run(debug=True)
