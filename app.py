47d8959964b9d6e6855ad690f2d765d1629782a0
jose santos <eng.josesantos97@gmail.com>
2026-03-01 23:50:27 +0100

    Add: a function to generate an API key and a decorator function to validate it

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, render_template, request
 from crypto import *
 import re
+import secrets
+import hashlib
+
 
 # This dictionary is just a simulation with static data
 # In the future, it will be updated automatically.
@@ -10,8 +13,29 @@ cryptos = {
     "ltc" : Crypto(3, "Litecoin", "LTC", 56.41),
     "sol" : Crypto(4, "Solana", "SOL", 88.28),
     "doge": Crypto(5, "Dogecoin", "DOGE", 0.11)
-}       
+}   
+API_KEY = ""    
 app = Flask(__name__)
+
+def generate_api_key():
+    #random_string = secrets.token_hex(32)
+    #return hashlib.sha256(random_string.encode()).hexdigest()
+    return secrets.token_hex(16)
+
+def require_api_key(f):
+    def decorated(*args, **kwargs):
+        key = request.headers.get("x-api-key")
+
+        if not key:
+            return jsonify({"error": "API key is missing"}), 401
+
+        if key != API_KEY:
+            return jsonify({"error": "Invalid API key"}), 403
+
+        return f(*args, **kwargs)
+
+    decorated.__name__ = f.__name__
+    return decorated
 
 @app.route("/")
 def index():
@@ -52,6 +76,7 @@ def contact():
 
 
 @app.route("/token/<string:code>")
+@require_api_key
 def get_crypto(code):
     crypto = cryptos.get(code.lower())
 
@@ -67,4 +92,6 @@ def get_tokens():
     })
 
 if __name__ == "__main__":
+    API_KEY = generate_api_key()
+    print(API_KEY)
     app.run(debug=True)
